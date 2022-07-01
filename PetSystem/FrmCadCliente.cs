f0c25439b12249b6b03cd39dccd9ee1f3dfed3a6
brbvi <vito.borba@gmail.com>
2022-07-01 14:42:34 -0300

    finalized FrmCadCliente with functions

--- PetSystem/FrmCadCliente.cs
+++ PetSystem/FrmCadCliente.cs
@@ -13,5 +13,52 @@ namespace PetSystem {
         public FrmCadCliente() {
             InitializeComponent();
         }
+        
+        private void clienteBindingNavigatorSaveItem_Click(object sender, EventArgs e) {
+            try {
+                this.Validate();
+                this.clienteBindingSource.EndEdit();
+                clienteTableAdapter.Update(petshopDataSet.cliente);
+                groupBox1.Enabled = false;
+                MessageBox.Show("Registro salvo!");
+            } catch (Exception) {
+                MessageBox.Show("Ocorreu um erro, verifique os valores informados");
+               
+            }
+
+        }
+
+        private void FrmCadCliente_Load(object sender, EventArgs e) {
+            // TODO: esta linha de código carrega dados na tabela 'petshopDataSet.cliente'. Você pode movê-la ou removê-la conforme necessário.
+            this.clienteTableAdapter.Fill(this.petshopDataSet.cliente);
+
+        }
+
+        // Adicionar novo cliente.
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e) {
+            clienteBindingSource.AddNew();
+            groupBox1.Enabled = true;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e) {
+            clienteBindingSource.CancelEdit();
+            groupBox1.Enabled = false;
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e) {
+            groupBox1.Enabled = true;
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e) {
+            try {
+                if (MessageBox.Show("Confirme exclusão do registro", "PetSystem", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
+                    clienteBindingSource.RemoveCurrent(); // remove
+                    clienteTableAdapter.Update(petshopDataSet.cliente); // salva
+                }
+            } catch (Exception) {
+                clienteTableAdapter.Fill(petshopDataSet.cliente);
+                MessageBox.Show("Registro não pode ser excluido");
+            }
+        }
     }
 }
